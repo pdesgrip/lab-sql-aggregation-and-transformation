29e2412c1971702b1c2d27d645828f000850615a
Paul Desgrippes <pdesgrip@gmail.com>
2023-09-22 17:49:26 +0200

    solved_lab

--- sql-aggregation-and-transformation.sql
+++ sql-aggregation-and-transformation.sql
@@ -1,3 +1,6 @@
+-- Challenge 1
+
+
 USE sakila;
 
 -- Show all max and min film durations
@@ -22,6 +25,9 @@ FROM sakila.film
 ORDER BY title ASC;
 
 
+
+
+
 -- Retrieve rental information and add an additional column called DAY_TYPE with values 'weekend' or 'workday', depending on the day of the week. Hint: use a conditional expression
 
 SELECT *, IF(DAYOFWEEK(rental_date) IN (1, 7), 'weekend', 'workday') AS DAY_TYPE;
@@ -31,5 +37,63 @@ SELECT *, IF(DAYOFWEEK(rental_date) IN (1, 7), 'weekend', 'workday') AS DAY_TYPE
 SELECT rental_duration, IF(rental_duration IS NULL, 'Not Available', rental_duration) AS rental_duration
 FROM sakila.film
 ORDER BY rental_duration ASC;
+
+-- Concatenates first and last names and returns first 3 letters of email address
+
+SELECT CONCAT(first_name, ' ', last_name) AS "Customer Name", LEFT(email, 3) AS "Email Prefix"
+FROM sakila.customer
+ORDER BY last_name ASC;
+
+-- Challenge 2
+
+-- Total Number of films released grouped by rating
+
+SELECT COUNT(*) AS "Total Number of Films"
+FROM sakila.film;
+
+-- Number of films by rating
+
+SELECT rating, COUNT(*) AS "Number of Films"
+FROM sakila.film
+GROUP BY rating;
+
+
+-- Number of films for each rating sorted
+
+SELECT rating, COUNT(*) AS "Number of Films"
+FROM sakila.film
+GROUP BY rating
+ORDER BY COUNT(*) DESC;
+
+-- Rentals by staff
+
+SELECT staff_id, COUNT(*) AS "Number of Rentals Processed"
+FROM sakila.rental
+GROUP BY staff_id
+ORDER BY COUNT(*) DESC;
+
+-- Mean film duration
+
+SELECT rating, ROUND(AVG(length), 2) AS "Mean Film Duration"
+FROM sakila.film GROUP BY rating
+ORDER BY "Mean Film Duration" DESC;
+
+-- Ratings with mean above 2 hours
+
+SELECT rating, ROUND(AVG(length), 2) AS "Mean Film Duration"
+FROM sakila.film
+GROUP BY rating
+HAVING AVG(length) > 120;
+
+
+-- Last names not repeated in Actor table
+
+SELECT last_name
+FROM sakila.actor
+GROUP BY last_name
+HAVING COUNT(last_name) = 1;
+
+
+
 
 
