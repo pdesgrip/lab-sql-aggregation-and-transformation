23a03e730afd6b0da900c4c0fbd944373cd9e290
Paul Desgrippes <pdesgrip@gmail.com>
2023-09-22 17:33:39 +0200

    Version_2

--- sql-aggregation-and-transformation.sql
+++ sql-aggregation-and-transformation.sql
@@ -2,16 +2,12 @@ USE sakila;
 
 -- Show all max and min film durations
 
-SELECT 
-    MIN(length) AS min_duration, 
-    MAX(length) AS max_duration 
+SELECT MIN(length) AS min_duration, MAX(length) AS max_duration 
 FROM sakila.film;
 
 -- The average film duration in hours and minutes
 
-SELECT 
-    FLOOR(AVG(length) / 60) AS avg_duration_hours,
-    ROUND(AVG(length) % 60) AS avg_duration_minutes 
+SELECT FLOOR(AVG(length) / 60) AS avg_duration_hours, ROUND(AVG(length) % 60) AS avg_duration_minutes 
 FROM sakila.film;
 
 -- Calculate time business in operation
@@ -21,7 +17,19 @@ FROM sakila.rental;
 
 -- Retrieve rental information and add two additional columns to show the month and weekday of the rental. Return 20 rows of results
 
-SELECT *, MONTH(rental_date) AS rental_month, DAYNAME(rental_date) AS rental_weekday 
-FROM sakila.rental
-LIMIT 20;
+SELECT title AS "Film Title", IF(rental_duration IS NULL, 'Not Available', CAST(rental_duration AS CHAR)) AS "Rental Duration (days)"
+FROM sakila.film
+ORDER BY title ASC;
+
+
+-- Retrieve rental information and add an additional column called DAY_TYPE with values 'weekend' or 'workday', depending on the day of the week. Hint: use a conditional expression
+
+SELECT *, IF(DAYOFWEEK(rental_date) IN (1, 7), 'weekend', 'workday') AS DAY_TYPE;
+
+-- Returns list of movies by title amnd duration and if duration is NULL replace it with "Not avilable"
+
+SELECT rental_duration, IF(rental_duration IS NULL, 'Not Available', rental_duration) AS rental_duration
+FROM sakila.film
+ORDER BY rental_duration ASC;
+
 
